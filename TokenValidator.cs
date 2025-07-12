09dc5541f4de96f835c4ea01720593298cc8f446
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-12 11:05:43 +0300

    Add files via upload

---
+++ TokenValidator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project12_JsonWebToken.JWT
+{
+    public class TokenValidator
+    {
+        public ClaimsPrincipal ValidateJwtToken(string token) // jwt tokeni çözücez. token olusturmaın tersi
+        {
+            var tokenHandler = new JwtSecurityTokenHandler(); // generator classında en son cagirmistik
+            var key = Encoding.UTF8.GetBytes("20Derste20ProjeToken+-*/1234tokenJWT");
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,// security key tokenı için kullanılır.
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = "localhost",
+                    ValidateAudience = true,
+                    ValidAudience = "localhost",
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        } 
+    }
+}
