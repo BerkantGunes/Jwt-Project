76af6abf0cc5aefbbf873c3e5b9b6f601dc2cf2d
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-12 11:06:19 +0300

    Add files via upload

---
+++ FrmEmployee.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Project12_JsonWebToken
+{
+    partial class FrmEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVEmployee = new System.Windows.Forms.DataGridView();
+            this.rtbToken = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVEmployee
+            // 
+            this.DGVEmployee.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVEmployee.Location = new System.Drawing.Point(0, 0);
+            this.DGVEmployee.Name = "DGVEmployee";
+            this.DGVEmployee.RowHeadersWidth = 51;
+            this.DGVEmployee.RowTemplate.Height = 24;
+            this.DGVEmployee.Size = new System.Drawing.Size(800, 273);
+            this.DGVEmployee.TabIndex = 0;
+            // 
+            // rtbToken
+            // 
+            this.rtbToken.Location = new System.Drawing.Point(0, 296);
+            this.rtbToken.Name = "rtbToken";
+            this.rtbToken.Size = new System.Drawing.Size(800, 142);
+            this.rtbToken.TabIndex = 1;
+            this.rtbToken.Text = "";
+            // 
+            // FrmEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbToken);
+            this.Controls.Add(this.DGVEmployee);
+            this.Name = "FrmEmployee";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personel List";
+            this.Load += new System.EventHandler(this.FrmEmployee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVEmployee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVEmployee;
+        private System.Windows.Forms.RichTextBox rtbToken;
+    }
+}
